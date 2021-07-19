ca88e098983dee35e65c65ec6a91aa21ff7f644a
jessicapcruz <jessica_p_cruz@hotmail.com>
2021-07-18 22:35:19 -0300

    inicio cadastro e estrutura de projeto mvvm.

---
+++ app/src/main/java/br/com/example/sociallearn/view/CadastroCompartilharSobre.kt
@@ -0,0 +1,12 @@
+package br.com.example.sociallearn.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.example.sociallearn.R
+
+class CadastroCompartilharSobre : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_compartilhar_sobre)
+    }
+}
