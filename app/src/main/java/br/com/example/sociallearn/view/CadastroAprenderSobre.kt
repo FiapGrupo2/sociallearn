82e10e30bd96874af8494beec02e238f1a2c362c
jessicapcruz <jessica_p_cruz@hotmail.com>
2021-07-19 23:12:55 -0300

    navegação entre as telas de cadastro.

--- app/src/main/java/br/com/example/sociallearn/view/CadastroAprenderSobre.kt
+++ app/src/main/java/br/com/example/sociallearn/view/CadastroAprenderSobre.kt
@@ -3,10 +3,19 @@ package br.com.example.sociallearn.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.example.sociallearn.R
+import android.content.Intent
+import android.widget.Button
 
 class CadastroAprenderSobre : AppCompatActivity() {
+    lateinit var btnNext: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_aprender_sobre)
+
+        btnNext = findViewById(R.id.btnNextCompartilhar)
+        btnNext.setOnClickListener {
+            val intent = Intent(this, CadastroCompartilharSobre::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/br/com/example/sociallearn/viewmodel/CadastroViewModel.kt
@@ -0,0 +1,6 @@
+package br.com.example.sociallearn.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class CadastroViewModel : ViewModel() {
+}
